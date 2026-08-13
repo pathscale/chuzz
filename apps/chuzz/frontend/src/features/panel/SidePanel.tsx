4eaf7327d0799009165d4a7a6c5f6abbe012bcdf
meh <no-reply@pathscale.com>
2026-08-13 08:21:32 +0700

    refactor: render SidePanel with Layouts

--- apps/chuzz/frontend/src/features/panel/SidePanel.tsx
+++ apps/chuzz/frontend/src/features/panel/SidePanel.tsx
@@ -1,5 +1,4 @@
-import { Button, Icon } from "@pathscale/test-ui";
-import { Chip, Flex } from "@pathscale/ui";
+import { Button, Chip, Flex, Icon } from "@pathscale/test-ui";
 import { For, Show, type JSX } from "solid-js";
 import { useBrowser } from "~/stores/browser";
 import { t } from "~/stores/i18n";
