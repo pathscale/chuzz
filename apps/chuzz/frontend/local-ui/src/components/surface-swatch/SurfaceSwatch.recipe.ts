a5af6545b11866972bb956e59a64d4210bf417e6
meh <no-reply@pathscale.com>
2026-08-13 16:50:34 +0700

    fix(ui): contain appearance wheel swatches

--- apps/chuzz/frontend/local-ui/src/components/surface-swatch/SurfaceSwatch.recipe.ts
+++ apps/chuzz/frontend/local-ui/src/components/surface-swatch/SurfaceSwatch.recipe.ts
@@ -2,7 +2,7 @@ import { recipe } from "solid-layouts";
 
 export const surfaceSwatch = recipe({
   component: "surface-swatch",
-  element: "button",
+  element: "span",
   slots: { root: { base: "surface-swatch" } },
   props: {
     color: {},
